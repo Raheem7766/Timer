40399b9d41960c275dba27e5fac1959ed4212118
Macbook P{ro <macbookpro@Macbooks-MacBook-Pro.local>
2025-10-30 17:20:23 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import CountdownTimer from "./components/CountdownTimer";
+import React, { useEffect, useState } from "react";
+
+function App() {
+  return (
+    <>
+      <CountdownTimer />
+    </>
+  );
+}
+
+export default App;
