687e6a1177273d99c11df6b2641476c528c00ae0
Macbook P{ro <macbookpro@Macbooks-MacBook-Pro.local>
2025-11-03 18:16:02 +0500

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,7 @@
-// import CountdownTimer from "./components/CountdownTimer";
-// import React, { useEffect, useState } from "react";
-
-// function App() {
-//   return (
-//     <>
-//       {/* <CountdownTimer /> */}
-//       <h1>Hello World</h1>
-//     </>
-//   );
-// }
-
-// export default App;
-
 import React from 'react'
 
 export default function App() {
   return (
-    <div>Hello !</div>
+    <div>Hello slash!</div>
   )
 }
