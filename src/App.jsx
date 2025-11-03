aceb0d53abff4c055bfca0c99feaa5e72e3b2b8d
Macbook P{ro <macbookpro@Macbooks-MacBook-Pro.local>
2025-11-03 15:40:56 +0500

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import React, { useEffect, useState } from "react";
 function App() {
   return (
     <>
-      <CountdownTimer />
+      {/* <CountdownTimer /> */}
+      <h1>Hello World</h1>
     </>
   );
 }
